2fc8632e54112e5817326e1ee746bb18b81fde7b
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 21:46:30 +0530

    Add files via upload

---
+++ 2._Initialization.java
@@ -0,0 +1,33 @@
+package jagged_Array;
+
+public class Initialization
+{
+
+	public static void main(String[] args)
+	{
+		
+int num[][]=new int[3][];
+		
+		num[0]=new int[3];
+		num[1]=new int[2];
+		num[2]=new int[1];
+		
+		num[0][0]=1;
+		num[0][1]=1;
+		num[0][2]=1;
+		num[1][0]=1;
+		num[1][1]=1;
+		num[2][0]=1;
+		
+		for(int i=0;i<num.length;i++)
+		{
+			for(int j=0;j<num[i].length;j++)
+			{
+				System.out.println("num["+i+"]["+j+"]= "+num[i][j]);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
